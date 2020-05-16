acb890fff6dfeb37c48a892c8313909b1adfafa5
skrepka <john@doe.org>
2020-05-16 16:41:44 +0300

    fix task 4

--- s3/task4/query.go
+++ s3/task4/query.go
@@ -2,23 +2,41 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	_ "github.com/lib/pq"
 )
 
-func main() {
-	connStr := "user=postgres dbname=postgres sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
+type good struct {
+	id    int
+	item  string
+	price int
+}
 
+func (p *good) Get(name string, connect string) error {
+	db, err := sql.Open("postgres", connect)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	rows, err := db.Query("SELECT * FROM books")
+	rows, err := db.Query("SELECT * FROM goods WHERE item='" + name + "'")
+	if err != nil {
+		log.Fatal(err)
+	}
+	rows.Next()
+	err = rows.Scan(&p.id, &p.item, &p.price)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer rows.Close()
 
+	return nil
+}
+
+func main() {
+	connStr := "user=postgres dbname=postgres sslmode=disable"
+	var Milk good
+	Milk.Get("milk", connStr)
+	fmt.Print(Milk)
 }
